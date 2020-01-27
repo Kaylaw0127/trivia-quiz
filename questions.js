057cf6897336fe8f7463e3a19d56e925771a42da
Kayla Ward <kaylaw0127@gmail.com>
2020-01-26 21:00:45 -0800

    added questions

---
+++ questions.js
@@ -0,0 +1,38 @@
+var questions = [
+    {
+        question : "What is the HTML tag under which one can write the JavaScript code??",
+        optionA : "<javascript>",
+        optionB : "<script>",
+        optionC : "<js>",
+        optionD : "<style>",
+        correct : "B"
+    } ,   {
+       question : "What is the correct syntax for referring to an external script called “script.js”?",
+        optionA: "<script src=”geek.js”>",  
+        optionB: "<script href=”geek.js”>",
+        optionC: "<script ref=”geek.js”>",
+        optionD: "<script name=”geek.js”>",        
+        correct : "A"
+   } ,  {
+       question : 'How do you write "Hello World" in an alert box?',
+       optionA : 'msgBox("Hello World)',
+        optionB : 'alert("Hello World")',
+        optionC : 'alertBox("Hello World")',
+        optionD : 'msg("Hello World")',
+        correct : "B"
+   } , {
+       question : "Which operator is used to assign a value to a variable?",
+       optionA : "X",
+        optionB : ">",
+        optionC : ".",
+        optionD : "=",
+        correct : "D"
+   } , {
+       question : "How does a FOR loop start?",
+       optionA : "for i=1 to 5",
+        optionB : "for (i=0; i<=5)",
+        optionC : "for (i=0; i<=5; i++)",
+        optionD : "for (i<=5; i++)",
+        correct : "C"
+   } ,
+  ]
---
+++ script.js
@@ -0,0 +1,21 @@
+var startBtn = document.getElementById('startBtn')
+var questions = document.getElementById('question')
+startBtn.addEventListener('click', startGame)
+
+var questions = []
+    
+
+function startGame(){
+    console.log('started')
+    startBtn.classList.add('hide')
+    nextQuestion()
+}
+
+function nextQuestion(){
+    
+}
+
+function selectAnswer(){
+
+}
+
